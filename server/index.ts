283566a69ee243086335515b0e5cff4189eb8fde
PoyangLiu <liupo78@gmail.com>
2021-07-14 02:18:07 -0400

    fix cors error

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,18 @@
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes';
 import env from './environment';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(routes);
 
 app.listen(env.PORT, () => {
